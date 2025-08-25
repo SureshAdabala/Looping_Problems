a22dbc0e1a90472674bae8810bf41612add2cf0a
SureshAdabala <sureshadabala0836@gmail.com>
2025-08-25 18:23:14 +0530

    Looping problems start

---
+++ Pattern2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Pattern2
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number: ");
+        int num=sc.nextInt();
+        for(int i=1;i<=num;i++)
+        {
+            for(int j=1;j<=num;j++)
+            {
+                System.out.print(j+" ");
+            }System.out.println();
+        }
+    }
+}
