8d832cf8b459d389b40a196a1637d144de999319
SureshAdabala <sureshadabala0836@gmail.com>
2025-08-26 18:11:16 +0530

    8

---
+++ Pattern8.java
@@ -0,0 +1,32 @@
+//- - - - * 
+//- - - * - *
+//- - * - - - *
+//- * - - - - - *
+//* * * * * * * * *
+public class Pattern8
+{
+    public static void main(String[] args)
+    {
+        //rows
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=5;j>i;j--)
+            {
+                System.out.print("- ");
+            }
+            
+            for(int k=1;k<=2*i-1;k++)
+            {
+                if(k==1 || k==2*i-1 || i==5)
+                {
+                    System.out.print("* ");
+                }
+                else
+                {
+                    System.out.print("- ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
